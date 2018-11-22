697b7b8393b8decf80a469ab199324e61fe07f1a
Vladislav Sosiuk <vladislavsosiuk@gmail.com>
2018-11-22 11:24:14 +0200

    Create user interface for LoginForm

--- UserInterface/Forms/Login.cs
+++ UserInterface/Forms/Login.cs
@@ -16,5 +16,18 @@ namespace UserInterface.Forms
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age = Convert.ToInt32(textBox1.Text);
+            if (age >= 18)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Еще юн щегол!");
+            }
+        }
     }
 }
