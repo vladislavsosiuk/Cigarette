3c908c0ceb07d5b936e977b33bddab5e4cb8ed5a
Vladislav Sosiuk <vladislavsosiuk@gmail.com>
2018-11-26 11:12:42 +0200

    Create Post class

---
+++ UserInterface/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInterface.Models
+{
+    public class Post
+    {
+
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+}
