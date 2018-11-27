ccfb40f9a48d3a675a6738d8406737c41d5d0860
Vladislav Sosiuk <vladislavsosiuk@gmail.com>
2018-11-27 10:49:21 +0200

    Log when Main form loaded

--- UserInterface/Forms/MainForm.cs
+++ UserInterface/Forms/MainForm.cs
@@ -22,6 +22,7 @@ namespace UserInterface.Forms
 
         private async void MainForm_Load(object sender, EventArgs e)
         {
+            System.Diagnostics.Debug.WriteLine("main form has been loaded");
             WebClient client = new WebClient();
             string result = await client.DownloadStringTaskAsync("https://jsonplaceholder.typicode.com/posts");
             List<Post> posts = JsonConvert.DeserializeObject<List<Post>>(result);
